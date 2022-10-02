f0bfe8bf15d3e22e9242f20f690e469ac9ba160f
johnahnz0rs <johnahn@z0rs.org>
2022-10-02 16:17:40 -0700

    started voting results

--- src/views/QuickPick.vue
+++ src/views/QuickPick.vue
@@ -1,121 +1,174 @@
 <template>
+  <div id="page-quickpick">
 
-  <!-- page heading -->
-  <div id="phase-selector">
-    <h1>quick pick: step {{ view }}</h1>
-    <div id="arrows">
-      <v-btn v-if="view > 1" @click="previousStep">⇦ Prev Step</v-btn>
-      <span v-else>&nbsp;</span>
-      <v-btn v-if="view <= 3" @click="nextStep">Next Step ⇨</v-btn>
-    </div>
-  </div>
 
-        <v-btn @click="devprint(nominatedMovies)">print nominatedMovies</v-btn>
-  
-  <!-- step 1: nominations -->
-  <div v-if="view == 1" id="step-1">
-    <div id="step-1-info">
-      <h2>nominate movies</h2>
-      <p>search for the movie you want to nominate and click "nominate" on your choice(s). when movie nominations are done, click "next step" above.</p>
-    </div>
+    <!-- page header --> 
+    <!-- page header --> 
+    <!-- page header --> 
+    <div id="phase-selector">
+      <h1>quick pick: step {{ view }}</h1>
 
-    <!-- step 1-A: search -->
-    <div id="search-fields">
-      <v-row>
-        <v-col cols="8" md="10"><v-text-field @keyup.enter="getMovies" outlined v-model="searchTerm" placeholder="Search movies"></v-text-field></v-col>
-        <v-col cols="4" md="2"><v-btn @click.prevent="getMovies">Search</v-btn></v-col>
-      </v-row>
-    </div>
-
-    <!-- step 1-B: nominate movies -->
-    <div v-if="resultData">
-      <div v-for="movie in resultData" :key="movie.id" class="movie-card">
-        <!-- {{ movie }} -->
-        <p><span class="movie-title">{{ movie.title }} </span><span v-if="movie.release_date">({{ movie.release_date.slice(0,4) }})</span></p>
-        <img v-if="movie.poster_path" class="inline-movie-data" :src="`https://image.tmdb.org/t/p/w154/${movie.poster_path}`">
-        <p v-if="movie.overview" class="inline-movie-data">{{ movie.overview.slice(0,300) }}...</p>
-        <v-btn @click="nominateMovie(movie)" :disabled="nominatedMovies.indexOf(movie) > -1 ? true : false">{{ nominatedMovies.indexOf(movie) > -1 ? "Nominated" : "Nominate this movie" }}</v-btn>
+      <!-- info --> <!-- THIS SHOULD BE A COMPONENT -->
+      <!-- info --> <!-- THIS SHOULD BE A COMPONENT -->
+      <!-- info --> <!-- THIS SHOULD BE A COMPONENT -->
+      <div id="info-about-this-step">
+        <div v-if="view == 1">
+          <h2>search & nominate movies</h2>
+          <p>search for the movie you want to watch and click "nominate" (minimum 2).</p>
+          <p>when you're ready, click "confirm" to review and vote.</p>
+        </div>
+        <div v-else-if="view == 2">
+          <h2>review nominations</h2>
+          <p>everything look good? ready to vote?</p>
+        </div>
+        <div v-else-if="view == 3">
+          <h2>Vote for y'alls's movie</h2>
+          <p><strong>one voter at a time:</strong></p>
+          <p>make your selection. click "Finished" when everyone has voted. </p>
+        </div>
       </div>
-    </div>
-  </div>
+      
+      
+      <!-- nav arrows (between the steps) --> <!-- THIS SHOULD BE A COMPONENT --> 
+      <!-- nav arrows (between the steps) --> <!-- THIS SHOULD BE A COMPONENT --> 
+      <!-- nav arrows (between the steps) --> <!-- THIS SHOULD BE A COMPONENT --> 
+      <div id="arrows"> 
+        <!-- previous step buttons -->
+        <span v-if="view == 1">&nbsp;</span>
+        <v-btn v-else-if="view == 2" @click="previousStep">⇦ Nominate <br>Movie(s)</v-btn>
+        <v-btn v-else-if="view == 3" @click="previousStep">⇦ Cancel <br>the Vote</v-btn>
+        <!-- next step buttons -->
+        <v-btn v-if="view == 1" @click="nextStep" :disabled="nominatedMovies.length == 0">Confirm <br>Nominations ⇨</v-btn>
+        <v-btn v-else-if="view == 2" @click="nextStep" :disabled="nominatedMovies.length < 2">Start <br>the Vote ⇨</v-btn>
+        <v-btn v-else-if="view == 3" @click="finishedVoting" :disabled="votes.length < 1">EVERYONE is <br>Finished Voting ⇨</v-btn>
+        <v-btn v-else-if="view == 4" @click="startFromScratch">Start from Scratch</v-btn>
+      </div>
+
+      <v-btn @click="devprint(tempVote)">print tempVote</v-btn>
+    </div> <!-- end page header -->
 
 
+  
+    <!-- step 1: nominations --> <!-- THIS SHOULD BE A COMPONENT -->
+    <!-- step 1: nominations --> <!-- THIS SHOULD BE A COMPONENT -->
+    <!-- step 1: nominations --> <!-- THIS SHOULD BE A COMPONENT -->
+    <div v-if="view == 1" id="step-1">
+
+      <!-- step 1-A: search -->
+      <div id="search-fields">
+        <v-row>
+          <v-col cols="8" md="10"><v-text-field @keyup.enter="getMovies" outlined v-model="searchTerm" placeholder="Search movies"></v-text-field></v-col>
+          <v-col cols="4" md="2"><v-btn @click.prevent="getMovies">Search</v-btn></v-col>
+        </v-row>
+      </div>
+
+      <!-- step 1-B: nominate movies -->
+      <div v-if="resultData">
+        <v-row>
+          <v-col v-for="movie in resultData" :key="movie.id" class="movie-card" cols="6" md="4">
+            <p class="movie-title">
+              <span>{{ movie.title }} </span>
+              <span v-if="movie.release_date">({{ movie.release_date.slice(0,4) }})</span>
+            </p>
+            <img v-if="movie.poster_path" :src="`https://image.tmdb.org/t/p/w154/${movie.poster_path}`">
+            <p v-if="movie.overview">{{ movie.overview.slice(0,150) }}...</p>
+            <v-btn @click="nominateMovie(movie)" :disabled="nominatedMovies.indexOf(movie) > -1 ? true : false">{{ nominatedMovies.indexOf(movie) > -1 ? "Nominated" : "Nominate" }}</v-btn>
+          </v-col>
+        </v-row>
+      </div>
 
 
-
-  <!-- step 2: confirm nominations -->
-  <div v-if="view==2" id="step-2">
-    <div id="step-2-info">
-      <h2>review nominations</h2>
-      <p>click "prev step" to nominate more movies</p>
-      <p>click "next step" if everything looks good</p>
-    </div>
-
-    <v-row v-if="nominatedMovies.length > 1">
-      <v-col cols="5" v-for="movie in nominatedMovies" :key="movie.id">
-        <p><strong>{{ movie.title }}</strong></p>
-        <img v-if="movie.poster_path" class="inline-movie-data" :src="`https://image.tmdb.org/t/p/w154/${movie.poster_path}`"><br />
-        <v-btn @click="removeNomination(movie)">remove</v-btn>
-      </v-col>
-    </v-row>
-    <div v-else>
-      <p id="not-enough-noms">not enough movies nominated; please click "prev step" to nominate more</p>
-    </div>
-  </div>
-
-
-
-
-
-
-
-
-
-
-  <!-- steep 3: how many attendees? -->
-  <div v-if="view == 3" id="step-3">
-    <h2>how many watchers/voters?</h2>
-
-    <div>
-      <v-text-field outline v-model="numberOfVoters" type="number" label="How many voters?" min=2></v-text-field>
-      <!-- <v-text-field outline v-model="numberOfVotesPerVoter" type="number" label="How many choices can each attendee make?" min=0 :max="nominatedMovies.length"></v-text-field> -->
-    </div>
-  </div>
-
-
-
-
-
-
-  <!-- step 4: start voting -->
-  <div v-if="view == 4" id="step-4">
-    <h2>Vote for tonight's movie</h2>
-
-    <div v-for="voterNumber in Object.keys(voters)" :key="voterNumber">
-        {{voterNumber}}lol
     </div>
 
 
 
 
 
+    <!-- step 2: confirm nominations --> <!-- THIS SHOULD BE A COMPONENT -->
+    <!-- step 2: confirm nominations --> <!-- THIS SHOULD BE A COMPONENT -->
+    <!-- step 2: confirm nominations --> <!-- THIS SHOULD BE A COMPONENT -->
+    <div v-if="view==2" id="step-2">
+
+      <div v-if="nominatedMovies.length < 2">
+        <p id="not-enough-noms">not enough movies nominated; please click "prev step" to nominate more</p>
+      </div>
+
+      <div>
+        <v-row>
+          <v-col v-for="movie in nominatedMovies" :key="movie.id" cols="6" md="4">
+            <p class="movie-title"><strong>{{ movie.title }}</strong></p>
+            <img v-if="movie.poster_path" :src="`https://image.tmdb.org/t/p/w154/${movie.poster_path}`"><br />
+            <v-btn @click="removeNomination(movie)">remove</v-btn>
+          </v-col>
+        </v-row>
+      </div>
+      
+      
+    </div>
 
 
 
-    <v-row v-if="nominatedMovies.length" style="display: none;">
-      <v-col v-for="movie in nominatedMovies" :key="movie.id" class="movie-card">
-        <!-- {{ movie }} -->
-        <img class="inline-movie-data" :src="`https://image.tmdb.org/t/p/w154/${movie.poster_path}`">
-        <p>{{ movie.title }} ({{ movie.release_date.slice(0,4) }})</p>
-        
-        <p class="inline-movie-data">{{ movie.overview.slice(0,300) }}...</p>
-        <!-- <v-btn @click="clickedAMovie(movie)">Nominate this movie</v-btn> -->
-      </v-col>
-    </v-row>
-  </div>
+
+
+    <!-- step 3: start voting --> <!-- THIS SHOULD BE A COMPONENT -->
+    <!-- step 3: start voting --> <!-- THIS SHOULD BE A COMPONENT -->
+    <!-- step 3: start voting --> <!-- THIS SHOULD BE A COMPONENT -->
+    <div v-if="view == 3" id="step-3">
+      
+      <!-- {{ devprint('tempVoter') }}
+      {{ devprint(tempVoter) }} -->
+      <v-row>
+        <v-col>
+          <h3>VOTER {{ votes.length + 1 }}</h3>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col><v-btn @click="resetMyVote">Reset <br>Voter {{ votes.length + 1}}</v-btn></v-col>
+        <v-col><v-btn @click="submitMyVote">Voter {{ votes.length + 1 }} done. <br>NEW VOTER.</v-btn></v-col>
+      </v-row>
+      
+      <v-row>
+
+        <v-col v-for="movie in nominatedMovies" :key="movie.id" cols="6" md="4" :class="tempVote.id == movie.id ? 'my-vote' : ''">
+          <!-- {{ movie }} -->
+          <img :src="`https://image.tmdb.org/t/p/w154/${movie.poster_path}`">
+          <p>{{ movie.title }} ({{ movie.release_date.slice(0,4) }})</p>
+          <p>{{ movie.overview.slice(0,120) }}...</p>
+          <v-btn :id="movie.id" class="vote-button" @click="myVote(movie)" :disabled="'id' in tempVote">{{ tempVote.id == movie.id ? 'MY VOTE' : 'Vote for this' }}</v-btn>
+        </v-col>
+
+      </v-row>
+
+
+    </div> <!-- end vote -->
 
 
 
+
+    <!-- step 4: vote results -->
+    <div v-if="view == 4">
+      <p>vote results</p>
+
+      <!-- <p v-for=""></p> -->
+    </div>
+
+
+
+
+
+    
+    <!-- steep 3: how many attendees? --> <!-- ja note 2022-10-02 - i realized i can just add each voter's data as an object, to an array, on the fly. so i don't need a pre-determined datafield to clog this up jah bless  -->
+    <!-- <div v-if="view == 3" id="step-3"> -->
+      <!-- <h2>how many watchers/voters?</h2> -->
+      <!-- <div> -->
+        <!-- <v-text-field outline v-model="numberOfVoters" type="number" label="How many voters?" min=2></v-text-field> -->
+      <!-- </div> -->
+    <!-- </div> -->
+
+
+
+
+  </div> <!-- end page -->
 </template>
 
 <script>
@@ -123,7 +176,7 @@ export default {
   name: 'QuickPick',
   data() {
     return {
-      view: 4,
+      view: 3,
       tmdbApi: '4b82ea454a84cdf8315e0146aa0aea00',
       searchTerm: '',
       resultData: [],
@@ -177,25 +230,25 @@ export default {
           vote_count: 7140
         },
       ],
-      numberOfVoters: 2,
-      
-      // numberOfVotesPerVoter: 0,
+      votes: [],
+      tempVote: {},
+      voteResults: [],
     };
   },
-  computed: {
-    voters() {
-      let vA = {};
-      for (let i=1; i <= this.numberOfVoters; i++ ) {
-        vA[i] = {
-          firstChoice: ''
-        };
-      }
-      return vA;
-    }
-  },
+  // computed: {},
   methods: {
     devprint(whatever) {
       console.log(whatever);
+    },
+    previousStep() {
+      if (this.view == 3) {
+        this.votes = [];
+        this.tempVote = {};
+      }
+      this.view--;
+    },
+    nextStep() {
+      this.view++;
     },
     async getMovies() {
       console.log('getMovies', this.searchTerm);
@@ -208,7 +261,7 @@ export default {
     nominateMovie(movie) {
       // console.log(`you clicked a movie, id: ${id}`);
       this.nominatedMovies.push(movie);
-      console.log(this.nominatedMovies);
+      // console.log(this.nominatedMovies);
     },
     removeNomination(movie) {
       const i = this.nominatedMovies.indexOf(movie);
@@ -216,18 +269,41 @@ export default {
         this.nominatedMovies.splice(i, 1);
       }
     },
-    previousStep() {
-      this.view--;
+    myVote(movie) {
+      this.tempVote = movie;
+      // console.log(movie);
     },
-    nextStep() {
+    resetMyVote() {
+      this.tempVote = {};
+    },
+    submitMyVote() {
+      this.votes.push(this.tempVote);
+      this.tempVote = {};
+    },
+    finishedVoting() {
+      // this.tempVote = {}
       this.view++;
+      for (let i in this.votes) {
+        console.log(this.votes[i]);
+        this.voteResults.push(this.votes[i].id);
+      }
+      
+    },
+    startFromScratch() {
+      this.voteResults = [];
+      this.tempVote = {};
+      this.votes = [];
+      this.nominatedMovies = [];
+      this.resultData = [];
+      this.searchTerm = '';
+      this.view == 1;
     }
   },
 }
 </script>
 <style lang="scss" scoped>
 #phase-selector {
-  text-align: center;
+  // text-align: center;
   #arrows {
     display: flex;
     justify-content: space-between;
@@ -237,35 +313,37 @@ export default {
   }
 }
 
+// seach for movies && click to nominate
 #step-1 {
-  #step-1-info {
-    // display: flex;
-    // justify-content: space-between;
-  }
-  #search-fields {
-    // display: flex;
-  }
+  #step-1-info {}
+  #search-fields {}
   .movie-card {
     border: 1px solid black;
     padding: 8px 12px;
-    margin: 8px;
-  }
-  .inline-movie-data {
-    display: inline-block;
+    // margin: 8px;
   }
   .movie-title {
     font-size: 1.8em;
     font-weight: bold;
-    /* padding-left: 8px; */
   }
 }
 
+// review noms
 #step-2 {
   #not-enough-noms {
     border: 1px solid red;
     padding: 8px;
     margin-top: 12px;
   }
+}
+
+// vote
+#step-3 {
+
+  .my-vote {
+    border: 1px solid red;
+  }
+
 }
 </style>
 
