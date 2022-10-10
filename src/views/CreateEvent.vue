f437e4a2bd214795a1d215d926845b1ee5daf2b1
johnahnz0rs <johnahn@z0rs.org>
2022-10-10 01:33:18 -0700

    fine-tuned movieNight object, added admin info

--- src/views/CreateEvent.vue
+++ src/views/CreateEvent.vue
@@ -29,7 +29,7 @@
       <div v-if="view == 1" id="event-details">
         <v-row>
           <v-col cols="12">
-            <v-text-field variant="outlined" label="Event Name (optional)" v-model="eventName"></v-text-field>
+            <v-text-field variant="outlined" label="Event Name (optional)" v-model="eventName" clearable></v-text-field>
           </v-col>
         </v-row>
         <v-row>
@@ -56,7 +56,7 @@
         </v-row>
         <v-row>
           <v-col cols="12">
-            <v-text-field variant="outlined" label="Location / Address" v-model="location"></v-text-field>
+            <v-text-field variant="outlined" label="Location / Address" v-model="location" clearable></v-text-field>
           </v-col>
         </v-row>
         <v-row id="view-actions">
@@ -71,20 +71,27 @@
       <!-- phase 2: invite friends -->
       <!-- phase 2: invite friends -->
       <!-- phase 2: invite friends -->
-      <div v-else-if="view == 2" id="event-invite-friends">
+      <div v-else-if="view == 2" id="event-invite-friends" >
+
 
         <v-row v-for="(friend, index) in friends" :key="index">
-          <v-col cols="1">{{ index + 1 }}.</v-col>
-          <v-col>
-            <!-- <span>lol</span> -->
-            <v-text-field v-model="friends[index].id" label="friend's phone number" placeholder="10 digits only" type="tel"></v-text-field>
-          </v-col>
-          <v-col cols="2" @click="removeFriend(index)" class="remove-friend">remove</v-col>
+          <v-card width="100%" variant="outlined" class="my-5">
+            <v-card-item class="pb-0">
+              <v-text-field v-model="friends[index].name" label="friend's name" density="compact"></v-text-field>
+              <v-text-field v-model="friends[index].id" label="friend's phone number" placeholder="10 digits only" type="tel" density="compact"></v-text-field>
+            </v-card-item>
+            <v-card-actions class="pt-0">
+              <v-btn @click="removeFriend(index)" variant="outlined" color="red" size="small" class="ml-auto">remove</v-btn>
+            </v-card-actions>
+          </v-card>
         </v-row>
 
-        <v-row>
-          <v-col id="invite-another-friend">
+
+        <v-row class="">
+          <!-- <v-col class="invite-another-friend"> -->
+          <v-col class="text-center">
             <v-btn @click="anotherFriend" variant="flat"><u>Invite another Friend</u></v-btn>
+            <p class="mt-5 pt-5"><em>{{ Object.keys(friends).length }} friends invited</em></p>
           </v-col>
         </v-row>
 
@@ -105,7 +112,7 @@
           <v-col>
             <v-slider v-model="nomsPerFriend" 
               :ticks="nomsPerFriendTickLabels"
-              :max="2"
+              :max="4"
               step="1"
               show-ticks="always"
               tick-size="5"
@@ -130,25 +137,41 @@
         <v-row>
           <v-col>
             <h2>{{ eventName ? eventName : 'Movie Night' }}</h2>
-            <p v-if="location">{{ location }}</p>
-            <p v-else class="missing-info"><span @click="view = 1">please enter location info</span></p>
-            <p v-if="month && day && year && hour && minute && meridian">{{ month }} {{ day }}, {{ year }} @ {{ hour }}:{{ minute }} {{ meridian }}</p>
-            <p v-else class="missing-info"><span @click="view = 1">please enter date & time</span></p>
+            <div v-if="location && month && day && year && hour && minute && meridian">
+              <p>{{ location }}</p>
+              <p>{{ month }} {{ day }} {{ year }} @ {{ hour }}:{{ minute }} {{ meridian}}</p></div>
+            <p v-else class="missing-info"><span @click="view = 1">please enter location, date, and time</span></p>
           </v-col>
         </v-row>
 
         <v-row>
           <v-col>
             <p><strong><em>each person can nominate up to {{ nomsPerFriend + 1 }} movie{{ nomsPerFriend == 0 ? '' : 's'}}</em></strong><br>&nbsp;</p>
-            <p v-if="hasFriendsInList">sending invites to:</p>
-            <p v-else class="missing-info"><span @click="view = 2">please add some friends</span></p>
-            <v-list v-if="hasFriendsInList">
-              <v-list-item v-for="(friend, index) in friends" :key="index">
-                {{ friend.id }}
-              </v-list-item>
-            </v-list>
           </v-col>
         </v-row>
+
+        <v-row>
+          <v-col>
+            <p v-if="hasFriendsInList" class="pb-3"><strong><em>sending invites to:</em></strong></p>
+            <p v-else class="missing-info"><span @click="view = 2">please invite at least 1 friend</span></p>
+            <ol v-if="hasFriendsInList" id="review-friends" class="ml-5">
+              <li v-for="(friend, index) in friends" :key="index" class="py-2">
+                {{ friend.name }} - {{ friend.id }}
+              </li>
+            </ol>
+          </v-col>
+        </v-row>
+
+
+        <!-- admin info -->
+        <v-row id="admin-info" class="mt-5">
+          <v-col>
+            <p><strong><em>we'll send you and your friends a link (via text msg) to pick a movie now!</em></strong></p>
+            <v-text-field v-model="admin.name" label="Your name" density="compact"></v-text-field>
+            <v-text-field v-model="admin.id" label="Your phone number" density="compact"></v-text-field>
+          </v-col>
+        </v-row>
+
 
         <v-row id="view-actions">
           <v-col>
@@ -169,13 +192,32 @@
 
 </template>
 <script>
+// import { mdiDelete } from '@mdi/js';
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// import { faPhone } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+// library.add(faPhone);
+
+// import { aliases, fa } from 'vuetify/iconsets/fa'
 // import firebase from 'firebase/compat/app';
 // $ curl 'https://api.rebrandly.com/v1/links/new?apikey=YOUR_API_KEY&destination=https://www.youtube.com/watch?v=3VmtibKpmXI&slashtag=video&domain\[id\]=8f104cc5b6ee4a4ba7897b06ac2ddcfb'
 // slashtag and title are optional
 export default {
+  // components: {
+  //   FontAwesomeIcon
+  // },
   name: 'CreateEvent',
   data() {
     return {
+      // constants
+      months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+      days: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
+      years: [2022, 2023, 2024],
+      hours: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+      minutes: ['00', '15', '30', '45'],
+      meridians: ['AM', 'PM'],
+      nomsPerFriendTickLabels: { 0: '1', 1: '2', 2: '3', 3: '4', 4: '5' },
+      // 
       uId: null,
       view: 1,
       eventName: null,
@@ -186,34 +228,27 @@ export default {
       minute: null,
       meridian: null,
       location: null,
-      friends: [{ id: '' }, { id: '' },],
+      friends: [{ id: '', name: '' }],
       nomsPerFriend: 0,
-      months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
-      days: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
-      years: [2022, 2023, 2024],
-      hours: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-      minutes: ['00', '15', '30', '45'],
-      meridians: ['AM', 'PM'],
-      nomsPerFriendTickLabels: {
-        0: '1',
-        1: '2',
-        2: '3'
-      },
+      admin: { id: '', name: '' },
+      
+      
     };
   },
   computed: {
     readyToSend() {
-      return this.uId && this.month && this.day && this.year && this.hour && this.minute && this.meridian && this.location;
+      return this.uId && this.month && this.day && this.year && this.hour && this.minute && this.meridian && this.location && this.admin.id.length && this.hasFriendsInList;
     },
     hasFriendsInList() {
-      return this.friends[0].id.toString().length && this.friends[1].id.toString().length;
+      // return this.friends[0].id.toString().length && this.friends[1].id.toString().length;
+      return this.friends[0].id.toString().length;
     }
   },
   methods: {
-    submitNewMovieNight() {
-      console.log("you submitted the form to create a new movie night");
-      console.log()
-    },
+    // submitNewMovieNight() {
+    //   console.log("you submitted the form to create a new movie night");
+    //   console.log()
+    // },
     updateMovieNightCookies() {
       console.log('***** updateMovieNightCookies() *****');
       const movieNight = {
@@ -227,7 +262,8 @@ export default {
         meridian: this.meridian,
         location: this.location,
         friends: this.friends,
-        nomsPerFriend: this.nomsPerFriend + 1,
+        admin: this.admin,
+        nomsPerFriend: this.nomsPerFriendTickLabels[this.nomsPerFriend],
       };
       const cookieString = JSON.stringify(movieNight);
       this.$cookies.set('movieNight', cookieString);
@@ -248,11 +284,14 @@ export default {
           this.hour = today.getHours();
         }
       }
+      if( this.hour < 10 ) {
+        this.hour = '0' + this.hour;
+      }
       this.minute = today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes();
       this.meridian = today.getHours() < 12 ? 'AM' : 'PM';
       this.location = 'Here, Now';
       this.updateMovieNightCookies();
-      this.view++;
+      // this.view++;
     },
     nextStep() {
       this.updateMovieNightCookies();
@@ -263,7 +302,7 @@ export default {
       this.view--;
     },
     anotherFriend() {
-      this.friends.push({ id: '' }); 
+      this.friends.push({ id: '', name: '' }); 
     },
     removeFriend(index) {
       console.log(index);
@@ -279,6 +318,7 @@ export default {
       }
       const movieNight = {
         uId: this.uId,
+        admin: this.admin,
         eventName: this.eventName,
         month: this.month,
         day: this.day,
@@ -288,13 +328,13 @@ export default {
         meridian: this.meridian,
         location: this.location,
         friends: cleanedFriends,
-        nomsPerFriend: this.nomsPerFriend + 1,
+        nomsPerFriend: this.nomsPerFriendTickLabels[this.nomsPerFriend],
       };
 
       this.$store.dispatch('events/createEvent', movieNight);
       this.$store.dispatch('votes/createVotes', movieNight);
       this.$store.dispatch('votes/sendAlerts', movieNight);
-      this.$cookies.remove('movieNight');
+      this.$cookies.remove('movieNight'); // this clears the cookies so the /create page will be a blank form jah bless
       this.$router.push(`votes/${movieNight.uId}/${movieNight.year}-${movieNight.month}-${movieNight.day}-${movieNight.hour}-${movieNight.minute}-${movieNight.meridian}/${movieNight.uId}`);
     },
   },
@@ -327,6 +367,34 @@ export default {
 };
 </script>
 <style scoped lang="scss">
+
+// material icons
+/* fallback */
+// @font-face {
+//   font-family: 'Material Icons';
+//   font-style: normal;
+//   font-weight: 400;
+//   src: url('../assets/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2') format('woff2');
+// }
+
+// .material-icons {
+//   font-family: 'Material Icons';
+//   font-weight: normal;
+//   font-style: normal;
+//   font-size: 24px;
+//   line-height: 1;
+//   letter-spacing: normal;
+//   text-transform: none;
+//   display: inline-block;
+//   white-space: nowrap;
+//   word-wrap: normal;
+//   direction: ltr;
+//   -webkit-font-feature-settings: 'liga';
+//   -webkit-font-smoothing: antialiased;
+// }
+
+
+
 #create-new-event {
   display: flex;
   justify-content: space-around;
@@ -347,7 +415,7 @@ export default {
       padding: 18px 0;
     }
 
-
+    // text
     .remove-friend {
       color: red;
       &:hover {
@@ -356,11 +424,12 @@ export default {
     }
 
 
-    #invite-another-friend {
-      display: flex; 
-      justify-content: space-around;
-      margin-bottom: 48px;
-    }
+    // button
+    // .invite-another-friend {
+    //   display: flex; 
+    //   justify-content: space-around;
+    //   margin-bottom: 48px;
+    // }
 
   
     #view-actions {
@@ -384,12 +453,19 @@ export default {
         padding: 0 8px;
         margin: 4px 0;
       }
+
+      #review-friends {
+        
+      }
     }
 
 
 
   }
 }
+
+
+
 
 
 </style>
--- src/views/VotesView.vue
+++ src/views/VotesView.vue
@@ -6,6 +6,33 @@
 </template>
 
 <script>
-export default {};
+export default {
+  // const uId = $route.params.uId;
+  // const date = $route.params.date;
+  // const friendId = $route.params.friendId;
+  data() {
+    return {
+      isAdmin: false,
+      uId: null,
+      date: null,
+      friendId: null,
+      movieNight: null,
+    };
+  },
+  created() {
+    this.uId = this.$route.params.uId;
+    this.date = this.$route.params.date;
+    this.friendId = this.$route.params.friendId;
+
+    // check if user is admin
+      // if user is admin, then have dashboard with admin actions (e.g. add/remove friends, change nomsPerFriend, change date, time)
+    // check if user is in the friends list
+    // check if nominations are finished
+    // check if votes are finished
+
+  },
+
+
+};
 </script>
 
