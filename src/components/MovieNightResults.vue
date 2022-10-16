fc432fbf7426841a47c07879847ee94889c2dd9d
johnahnz0rs <johnahn@z0rs.org>
2022-10-16 09:34:06 -0700

    started vote results

--- src/components/MovieNightResults.vue
+++ src/components/MovieNightResults.vue
@@ -1,6 +1,6 @@
 <template>
   <h2>MovieNightResults.vue</h2>
-
+  <p>results {{ results }}</p>
   
 </template>
 
@@ -13,6 +13,14 @@ export default {
   computed: {
     movieNight() {
       return this.$store.getters['movieNights/movieNight'];
+    },
+
+    results() {
+      let results = {};
+      if (this.movieNight.results) {
+        results = this.movieNight.results;
+      }
+      return results;
     },
   },
 
