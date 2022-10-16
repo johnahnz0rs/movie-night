96940b92c856c07e9a6323db9a29cbaa7cae10e1
johnahnz0rs <johnahn@z0rs.org>
2022-10-16 09:49:05 -0700

    con't vote results

--- src/components/MovieNightResults.vue
+++ src/components/MovieNightResults.vue
@@ -17,11 +17,49 @@ export default {
 
     results() {
       let results = {};
+
       if (this.movieNight.results) {
         results = this.movieNight.results;
+      } else {
+
+        // get all votes & sort into the 3 choices
+        const votes = this.movieNight.votes;
+        const allowedFriends = this.movieNight.allowedFriends;
+        let firstChoices = [];
+        let secondChoices = [];
+        let thirdChoices = [];
+        for (let i=0; i<allowedFriends.length; i++) {
+          const friendId = allowedFriends[i];
+          if (votes[friendId].first && votes[friendId].first.id) {
+            firstChoices.push(votes[friendId].first);
+          }
+          if (votes[friendId].second && votes[friendId].second.id) {
+            secondChoices.push(votes[friendId].second);
+          }
+          if (votes[friendId].third && votes[friendId].third.id) {
+            thirdChoices.push(votes[friendId].third);
+          }
+        }
+
+
+        // let weHaveAWinner = false;
+
+
+
+
       }
+      
+      
+
+
+
+      
+      // results = votes;
       return results;
     },
+
+
+
   },
 
 };
